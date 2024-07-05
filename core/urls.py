624ddcb4d5622582d7db22b0edc22f182819b2f1
Excellent58 <wycliffemusau99@gmail.com>
2024-07-05 18:33:02 +0300

    added new products section

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .import views
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
     path("signup/", views.signup, name="signup"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- emarket/settings.py
+++ emarket/settings.py
@@ -128,6 +128,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
+# media files
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
