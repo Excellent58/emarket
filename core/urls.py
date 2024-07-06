0ab74f7c1343931199f10fa88aecc5ff641b26f6
Excellent58 <wycliffemusau99@gmail.com>
2024-07-06 15:09:12 +0300

    create product detail page

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 from .import views
 
@@ -10,4 +8,4 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
     path("signup/", views.signup, name="signup"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
