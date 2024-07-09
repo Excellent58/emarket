ca9b2cca12814f12ba7e555ac6b9113fe635148b
Excellent58 <wycliffemusau99@gmail.com>
2024-07-09 15:06:22 +0300

    added edit functionality to user items

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 def detail(request, pk):
@@ -11,6 +11,7 @@ def detail(request, pk):
     return render(
         request, "item/detail.html", {"item": item, "related_items": related_items}
     )
+
 
 @login_required
 def new(request):
@@ -22,14 +23,42 @@ def new(request):
             item.created_by = request.user
             item.save()
 
-            return redirect('item:detail', pk=item.id)
+            return redirect("item:detail", pk=item.id)
     else:
         form = NewItemForm()
 
-    return render(request, 'item/form.html', {
-        'form': form,
-        'title': 'New item',
-    })
+    return render(
+        request,
+        "item/form.html",
+        {
+            "form": form,
+            "title": "New item",
+        },
+    )
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == "POST":
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("item:detail", pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(
+        request,
+        "item/form.html",
+        {
+            "form": form,
+            "title": "Edit item",
+        },
+    )
 
 
 @login_required
